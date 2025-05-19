75bfd4531654af55ed8f3b52c7d6bb9146aba666
swathi-sankaranarayanan <137164192+swathi-sankaranarayanan@users.noreply.github.com>
2025-05-19 11:12:27 +0000

    Comment formatting

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
        backend "local" {
        }
      }
-
-    # An example resource that does nothing.
-    resource "null_resource" "example" {
-        triggers = {
-            value = "A example resource that does nothing!"
+ 
+    #An example resource that does nothing.    
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
         }
-    }
+      }
