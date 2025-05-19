d3ca80fcb15f5a576565209c966be0a20c227a34
swathi-sankaranarayanan <137164192+swathi-sankaranarayanan@users.noreply.github.com>
2025-05-19 11:10:35 +0000

    Copy main.tf with format

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-    backend "local" {  
-    }
-}
+     terraform {
+       backend "local" {
+       }
+     }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-triggers = {
-    value = "A example resource that does nothing!"
+    # An example resource that does nothing.
+    resource "null_resource" "example" {
+        triggers = {
+            value = "A example resource that does nothing!"
+        }
     }
-}
