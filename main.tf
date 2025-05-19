6f13398b4d4f15f12dd0f388d88d26f3e7a77fcb
swathi-sankaranarayanan <137164192+swathi-sankaranarayanan@users.noreply.github.com>
2025-05-19 11:05:13 +0000

    Correct main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform 
+{
+    backend "local" 
+    {  }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+triggers = {
+    value = "A example resource that does nothing!"
+    }
+}
